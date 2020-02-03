42fdb3556ebed3e6e099537f9651ea7432433fef
danny.elliott <danny.elliott@nexusalphaa.com>
2020-02-03 15:15:48 +0000

    - started webpack config

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import './App.css';
 
 const App = () => (
-  <div className='app'>
-    <div className='card'>
-      <h1 className='title'>Our React is working :)</h1>
+    <div className='app'>
+        <div className='card'>
+            <h1 className='title'>Our React is working :)</h1>
+        </div>
     </div>
-  </div>
 );
 
 export default App;
